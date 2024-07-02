30f44ba31da58cc778fd6e61c7a1915e7e30938c
Šimon Nemčič <138312285+cicmen35@users.noreply.github.com>
2024-07-02 16:02:26 +0200

    Add files via upload
    Main code files.

---
+++ members/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def members(request):
+    my_members = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+      'mymembers': my_members
+    }
+    return HttpResponse(template.render(context, request))
